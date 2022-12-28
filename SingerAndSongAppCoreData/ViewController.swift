1547c33e36581a5c62b2b066a0334a757ea8361e
msnbek <mahmutsenbek@icloud.com>
2022-12-28 15:00:50 +0300

    Initial Commit

---
+++ SingerAndSongAppCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingerAndSongAppCoreData
+//
+//  Created by Mahmut Senbek on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
